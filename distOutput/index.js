f5601bf0eef0b237f6b56293086623d529dee976
tanishka <tsdineshjai@gmail.com>
2024-03-27 22:04:50 +0530

    csv project done

--- distOutput/index.js
+++ distOutput/index.js
@@ -1,50 +1,17 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/*
-CSV file be like this
-1: 1st row will be columns with headings
-eg: From, TO, Amount, Company
-2 : From now onwards it will be rows
-each row will be a record
-eg: Akshay, Smaay, 3000, Lioks
-
-*/
-const fs_1 = require("fs");
-class CSVWriter {
-    constructor(columns) {
-        this.columns = columns;
-        this.csv = this.columns.join(",") + "\n";
+class Players {
+    constructor(data) {
+        this.data = data;
     }
-    addRows(values) {
-        let rows = values.map((value) => this.formatRow(value));
-        this.csv += rows.join("\n");
-        console.log(this.csv);
+    logTheData() {
+        return this.data;
     }
-    saveToTheFile(filename) {
-        (0, fs_1.appendFileSync)(filename, this.csv);
-        this.csv = "\n";
-        console.log(`file has been saved`);
-    }
-    formatRow(p) {
-        return this.columns.map((col) => p[col]).join(",");
-    }
-    getCSVValue() {
-        console.log(this.csv);
+    filterOne(id) {
+        return this.data.filter((item) => item.id !== id);
     }
 }
-const newCSV = new CSVWriter(["id", "amount", "to", "notes"]);
-newCSV.addRows([
-    {
-        id: 121,
-        amount: 31313,
-        to: "madhes",
-        notes: "500Rs",
-    },
-    {
-        id: 2323,
-        amount: 5555,
-        to: "radhes",
-        notes: "1500Rs",
-    },
+const forwards = new Players([
+    { name: "chid", position: "forward", id: 23 },
+    { name: "nwaneri", position: "midfield", id: 33 },
 ]);
-newCSV.saveToTheFile("../data/payments.csv");
+console.log(forwards.logTheData());
--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,27 @@
-/*  
-CSV file be like this
-1: 1st row will be columns with headings
-eg: From, TO, Amount, Company
-2 : From now onwards it will be rows
-each row will be a record
-eg: Akshay, Smaay, 3000, Lioks
-
-*/
-import { appendFileSync } from "fs";
-
-interface Payment {
+interface HasId {
 	id: number;
-	amount: number;
-	to: string;
-	notes: string;
 }
+class Players<T extends HasId> {
+	constructor(private data: T[]) {}
 
-type PaymentColumns = ("id" | "amount" | "to" | "notes")[];
-
-class CSVWriter {
-	constructor(private columns: PaymentColumns) {
-		this.csv = this.columns.join(",") + "\n";
+	logTheData(): T[] {
+		return this.data;
 	}
 
-	private csv: string;
-
-	addRows(values: Payment[]): void {
-		let rows = values.map((value) => this.formatRow(value));
-
-		this.csv += rows.join("\n");
-
-		console.log(this.csv);
-	}
-
-	saveToTheFile(filename: string): void {
-		appendFileSync(filename, this.csv);
-
-		this.csv = "\n";
-
-		console.log(`file has been saved`);
-	}
-
-	private formatRow(p: Payment): string {
-		return this.columns.map((col) => p[col]).join(",");
-	}
-
-	getCSVValue(): void {
-		console.log(this.csv);
+	filterOne(id: number): T[] {
+		return this.data.filter((item: T) => item.id !== id);
 	}
 }
 
-const newCSV = new CSVWriter(["id", "amount", "to", "notes"]);
+interface Player {
+	name: string;
+	position: string;
+	id: number;
+}
 
-newCSV.addRows([
-	{
-		id: 121,
-		amount: 31313,
-		to: "madhes",
-		notes: "500Rs",
-	},
-	{
-		id: 2323,
-		amount: 5555,
-		to: "radhes",
-		notes: "1500Rs",
-	},
+const forwards = new Players<Player>([
+	{ name: "chid", position: "forward", id: 23 },
+	{ name: "nwaneri", position: "midfield", id: 33 },
 ]);
 
-newCSV.saveToTheFile("../data/payments.csv");
+console.log(forwards.logTheData());
